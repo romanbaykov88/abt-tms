ac1a368fe04a6e670f8fae98354bf07a4f671a20
Roman Baikov <romanbaykov@hotmail.com>
2023-04-30 18:30:46 -0400

    login with google

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/sheets/v4.dart';
+import 'package:googleapis_auth/googleapis_auth.dart' as auth show AuthClient;
+
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: <String>[SheetsApi.driveScope],
+);
 
 void main() {
   runApp(const MyApp());
+}
+
+/// The state of the main widget.
+class SignInDemoState extends State<MyHomePage> {
+  GoogleSignInAccount? _currentUser;
+  String _contactText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        _handleGetContact();
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Future<void> _handleGetContact() async {
+    setState(() {
+      _contactText = 'Loading contact info...';
+    });
+
+    // Retrieve an [auth.AuthClient] from the current [GoogleSignIn] instance.
+    final auth.AuthClient? client = await _googleSignIn.authenticatedClient();
+
+    assert(client != null, 'Authenticated client missing!');
+
+    // Prepare a People Service authenticated client.
+    final SheetsApi peopleApi = SheetsApi(client!);
+    // // Retrieve a list of the `names` of my `connections`
+    // final ListConnectionsResponse response =
+    // await peopleApi.people.connections.list(
+    //   'people/me',
+    //   personFields: 'names',
+    // );
+    //
+    // final String? firstNamedContactName =
+    // _pickFirstNamedContact(response.connections);
+    //
+    // setState(() {
+    //   if (firstNamedContactName != null) {
+    //     _contactText = 'I see you know $firstNamedContactName!';
+    //   } else {
+    //     _contactText = 'No contacts to display.';
+    //   }
+    // });
+  }
+
+  // String? _pickFirstNamedContact(List<Person>? connections) {
+  //   return connections
+  //       ?.firstWhere(
+  //         (Person person) => person.names != null,
+  //   )
+  //       .names
+  //       ?.firstWhere(
+  //         (Name name) => name.displayName != null,
+  //   )
+  //       .displayName;
+  // }
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error); // ignore: avoid_print
+    }
+  }
+
+  Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
+  Widget _buildBody() {
+    final GoogleSignInAccount? user = _currentUser;
+    if (user != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: user,
+            ),
+            title: Text(user.displayName ?? ''),
+            subtitle: Text(user.email),
+          ),
+          const Text('Signed in successfully.'),
+          Text(_contactText),
+          ElevatedButton(
+            onPressed: _handleSignOut,
+            child: const Text('SIGN OUT'),
+          ),
+          ElevatedButton(
+            onPressed: _handleGetContact,
+            child: const Text('REFRESH'),
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const Text('You are not currently signed in.'),
+          ElevatedButton(
+            onPressed: _handleSignIn,
+            child: const Text('SIGN IN'),
+          ),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Google Sign In + googleapis'),
+        ),
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: _buildBody(),
+        ));
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +142,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -44,7 +177,7 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => SignInDemoState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
